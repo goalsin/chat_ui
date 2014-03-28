7a6fc370e6c7a36ba98f992099c988266dfb9c6a
shiren1118 <shiren1118@126.com>
2014-03-28 15:52:22 +0800

    修改默认加载

--- chat/app/js/chat.js
+++ chat/app/js/chat.js
@@ -1,13 +1,13 @@
 $(function(){
+	//$('#url_pjax_chat').trigger('click');
+	$('#pjax-container').load('app/pages/messages.html');
 	
 	$('#url_pjax_chat').click(function(){
 		$('#pjax-container').load('app/pages/messages.html');
-		
 	});
 	
 	$('#url_pjax_write').click(function(){
 		$('#pjax-container').load('app/pages/write.html');
-		
 	});
 	
 	// 聊天多用户tab
