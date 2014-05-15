7697cbb0d2ed87164bf4235b601bb62a05b8bb61
shiren1118 <shiren1118@126.com>
2014-05-15 18:37:50 +0800

    add course info

--- chat/app/js/course_info.js
+++ chat/app/js/course_info.js
@@ -22,20 +22,17 @@ $(function(){
 			// login succ
 			if(res.data){
 				// alert(res.data);
-				var course_html = '';
+				var course_html = '';			
+				var course = res.data;
 				
-				
-					var course = res.data;
-					
-					course_html += "<li>"
-						+ "<a href='course.html?id=" + course.id + "'>"
-		                + "<img src='" + course.image + "' width='100%' height='170' title='案例：慕课网2048私人订制' alt='案例：慕课网2048私人订制'>"
-		                + "<h5>"+ course.name +"</h5>"
-		                + "<p>"+ course.desc.substring(0,30) +"</p>"
-		                + "<div class='tips'><span class='l'>讲师：" + course.author + "</span> <span class='r'>课程时长：" + course.week + "周</span></div>"
-		                + "</a>"
-		                + "</li>";
-			
+				course_html += "<li>"
+					+ ""
+	                + "<img src='" + course.image + "' width='100%' height='370' title='案例：慕课网2048私人订制' alt='案例：慕课网2048私人订制'>"
+	                + "<h5>"+ course.name +"</h5>"
+	                + "<p>"+ course.desc.substring(0,30) +"</p>"
+	                + "<div class='tips'><span class='l'>讲师：" + course.author + "</span> <span class='r'>课程时长：" + course.week + "周</span></div>"
+	                + ""
+	                + "</li>";
 				
 				$('.card').html(course_html);
 				
@@ -48,4 +45,5 @@ $(function(){
 	
 	//main 
 	get_course_info();
+	// $('#myTab a:last').tab('show')
 });
