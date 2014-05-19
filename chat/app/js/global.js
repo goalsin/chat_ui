dff925f98b0c6d612cccbac20d4dff1c90cc7499
alfred sang <goalsin@126.com>
2014-05-19 01:54:33 +0000

    token

--- chat/app/js/global.js
+++ chat/app/js/global.js
@@ -2,11 +2,12 @@ var DEV_HOST = "http://0.0.0.0:3000/";
 var SERVER_HOST = "http://at35.com:3000/";
 
 window.api_host = DEV_HOST;
+window.api_host = SERVER_HOST;
 
-window.token = "b08a9cf82227f5e03f46049960b2e104"
+window.token = "7e0140db91a9954d488286968e7c36de"
 /**
  * Query string parser
- */ 
+ */
 function getParameterByName(name) {
     name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
     var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
